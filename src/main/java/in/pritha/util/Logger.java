19aad6d0fc2cecb4875cd130b35486da736b9d2c
Pritha P <pritha.padmanathan@gmail.com>
2021-05-13 16:13:04 +0530

    Add Wedding Style Feature Done

--- src/main/java/in/pritha/util/Logger.java
+++ src/main/java/in/pritha/util/Logger.java
@@ -1,17 +1,10 @@
 package in.pritha.util;
 
-<<<<<<< HEAD
 public class Logger {
-     private Logger(){
-    	 //to avoid object creation
-     }
-=======
-public class Logger
-	
 	private Logger() {
-		//to avoid object creation
+		// to avoid object creation
 	}
->>>>>>> ce7ee7a1ae198774d5507cbc4c48951c3f64899a
+
 	/**
 	 * This method will print whatever the paramteres passed
 	 * 
